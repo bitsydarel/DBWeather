3cfb90e34a8cf75c21929034b0ba351c33b198e4
Darel Bitsy <bitsydarel@gmail.com>
2017-04-05 08:59:45 +0300

    Change in query handling

---
+++ app/src/main/java/com/darelbitsy/dbweather/model/UserCitiesDatabase.java
@@ -0,0 +1,10 @@
+package com.darelbitsy.dbweather.model;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Darel Bitsy on 04/04/17.
+ */
+
+public class UserCitiesDatabase extends SQLiteOpenHelper {
+}
