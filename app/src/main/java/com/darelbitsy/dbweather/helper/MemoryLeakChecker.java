d6f432c858144603a7e7076e00619078f4b888b4
Darel Bitsy <bitsydarel@gmail.com>
2017-04-06 16:51:51 +0300

    Added leakcanary library to check for memoryleak issue

---
+++ app/src/main/java/com/darelbitsy/dbweather/helper/MemoryLeakChecker.java
@@ -0,0 +1,10 @@
+package com.darelbitsy.dbweather.helper;
+
+import android.app.Application;
+
+/**
+ * Created by Darel Bitsy on 06/04/17.
+ */
+
+public class MemoryLeakChecker extends Application {
+}
